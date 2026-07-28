3e9f7f1ff550522a40b0a41df4ac941e5c6653f6
Krzysztof Kazor <krzysztof.kazor@gmail.com>
2026-07-28 11:19:44 +0200

    Form meta context

---
+++ registry/tanstack-form/context/form-meta-context.ts
@@ -0,0 +1,24 @@
+import { createContext, useContext } from "react";
+import {
+    RequiredAsterisk
+} from "../components/fields/field-label";
+
+interface FormMetaContext {
+  renderRequired: React.ReactNode | ((required?: boolean) => React.ReactNode);
+}
+
+export const formMetaContext = createContext<FormMetaContext>({
+  //   renderRequired: (required) => RequiredText({ required }),
+  renderRequired: RequiredAsterisk,
+});
+
+export const FormMetaProvider = formMetaContext.Provider;
+
+export function useFormMetaContext() {
+  const ctx = useContext(formMetaContext);
+  if (!ctx)
+    throw new Error(
+      "useFormMetaContext can be used only within FormMetaProvider/FormMeta component",
+    );
+  return ctx;
+}
