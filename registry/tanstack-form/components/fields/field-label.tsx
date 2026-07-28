c57cde572eb894a1829a8bf29588b99b40cd860a
Krzysztof Kazor <krzysztof.kazor@gmail.com>
2026-07-28 11:08:49 +0200

    Let user customize required component

--- registry/tanstack-form/components/fields/field-label.tsx
+++ registry/tanstack-form/components/fields/field-label.tsx
@@ -7,19 +7,29 @@ import { cn } from "@/lib/utils"
 export interface FormLabelProps {
     children: React.ReactNode,
     required?: boolean
+    renderRequired?: React.ReactNode | ((required?: boolean)=>React.ReactNode)
 }
 
-export function FieldLabel({ children, required }: Readonly<FormLabelProps>) {
+export function RequiredAstrix(){
+    return <span className="text-destructive ml-0.5">*</span>
+}
+
+export function RequiredText(){
+    return <span className="text-destructive ml-0.5">(required)</span>
+}
+
+export function FieldLabel({ children, required, renderRequired: _renderRequired }: Readonly<FormLabelProps>) {
     const field = useFieldContext<string>()
     const fieldErrors = useSelector(field.store, (state) => state.meta.errors)
-
+    const handleFaccRequired = () => typeof _renderRequired === 'function' ? _renderRequired(required) : _renderRequired
+    const renderRequired = _renderRequired ? handleFaccRequired() : <RequiredAstrix/>
     return (
         <Label htmlFor={field.name} className={cn(
             "text-sm font-medium",
             fieldErrors.length > 0 && 'text-destructive'
         )}>
             {children}
-            {required && <span className="text-destructive ml-0.5">*</span>}
+            {required && renderRequired}
         </Label>
     )
 }
